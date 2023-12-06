1a6dc713ca4ed40a543e11ecaa288cbc3b5bbb09
FranNinaa <francinesantoss85@gmail.com>
2023-12-05 23:47:12 -0300

    feito exercicio api-rest

---
+++ api-rest/src/app/controllers/UsuarioController.js
@@ -0,0 +1,55 @@
+import {buscarUsuarioID, deletarUsuarioId, getUsuarios} from ".app/database/conexao.js"
+
+class UsuarioController{
+
+    /**
+     * Lista Tudo
+     */
+    index(req, res) { 
+        res.status(200).send(getUsuarios())
+    }
+
+    /**
+     * Listar por Id
+     */
+    show(req, res){ 
+        let usuario = buscarUsuarioID(req.params.id)
+        res.status(200).json(usuario)
+    }
+
+    /**
+     * Salvar Novo Registro
+     */
+    store(req, res){
+        getUsuarios().push(req.body)//fazer a conversão para formato json
+        res.status(201).send("Usuário cadastrado com sucesso!")
+    }
+    
+    /**
+     * Atualizar Registro 
+     */
+    update(req, res){
+        let indexUsuario = buscarUsuarioID(req.params.id)
+        let usuarios = getUsuarios()
+        usuarios[indexUsuario].nome = req.body.nome
+        usuarios[indexUsuario].email = req.body.email
+        usuarios[indexUsuario].senha = req.body.senha
+        res.status(200).json(usuarios[indexUsuario])
+    };
+    
+
+
+    /**
+     * Deletar Registro
+     */
+    delete(req, res){
+        let indexUsuario = deletarUsuarioId(req.params.id);  // Corrigido para req.params.id
+        getUsuarios().splice(indexUsuario, 1)
+        res.status(200).send(`Usuario ${req.params.id} excluido com sucesso`);
+    };
+
+
+}
+
+//padrao Singleton (Design Patterns)
+export default new UsuarioController();
