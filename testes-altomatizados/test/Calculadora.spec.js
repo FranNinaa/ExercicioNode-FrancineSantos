0287e11d60b0310a0ec05c79012c0659d3d3f22c
FranNinaa <francinesantoss85@gmail.com>
2023-12-13 07:45:48 -0300

    feito os exemplos de testes na aula para melhorar o entendimento

--- testes-altomatizados/test/Calculadora.spec.js
+++ testes-altomatizados/test/Calculadora.spec.js
@@ -6,5 +6,26 @@ describe("Testes Calculadora.js", ()=>{
        let calc = new Calculadora()
        //invoca o método
          expect(calc.soma(1,2)).toEqual(3)
+         expect(typeof(resul)).toEqual("number")
+
     })
+
+    test("Funcao sub(a, b) - dado a = 1, b = 2, o resultado esperado eh -1", ()=>{
+        let calc = new Calculadora()
+        expect(calc.sub(1, 2)).toEqual(-1)
+    })
+
+    test("Funcao div(a, b) - dado a = 3 e b= 2, o resultado esperado eh 1.5", ()=>{
+        let calc = new Calculadora()
+        expect(calc.div(3, 2)).toBe(1.5)
+    })
+
+    test("Funcao div(a, b) - dado a = 3 e b= 0,  é esperado que lance o Erro 'Divisão por Zero'", ()=>{
+        let calc = new Calculadora()
+        expect(()=>{
+            calc.div(3, 0)
+        }).toThrow()
+    })
+
 })
+
